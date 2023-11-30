33119399c819be69ab0ad58c6a93b86d2dd2b666
senayakagunduz <senayakgndz@gmail.com>
2023-11-30 20:55:06 +0300

    ul  px-3 removed

--- src/app/components/SideBar.jsx
+++ src/app/components/SideBar.jsx
@@ -9,7 +9,7 @@ const SideBar = ({ collapsed }) => {
     return (
         <aside className={`flex bg-white sidebar relative top-0 bottom-0 z-99 transiton-width duration-300 overflow-y-auto ${collapsed ? 'w-20' : 'w-80 md:block'}  `}>
             <nav className='h-screen flex flex-col'>
-                <ul className="flex-1 px-3">
+                <ul className="flex-1">
                     {data.map((item, index) => (
                         <SideBarDynamic key={index} {...item} collapsed={collapsed} />
                     ))}
