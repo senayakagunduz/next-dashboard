d237e586d83cd881e95ec624642659054407dfde
senayakagunduz <senayakgndz@gmail.com>
2023-11-30 16:51:50 +0300

    comments deleted

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -59,22 +59,3 @@ const Header = ({ toggleSidebar }) => {
 }
 
 export default Header
-
-{/* // <nav className='flex w-100 h-50 items-center bg-white'>
-        //     <div className='flex justify-between'>
-        //             <div className='p-4 ps-4 pb-3 flex justify-between items-center'>
-        //                 <div className='p-4 ps-5 pb-3 flex items-center'>
-        //                     <img src="https://img.logoipsum.com/243.svg" className='w-32' alt="" />
-        //                 </div>
-        //                 <div className='p-4 pb-3'>
-        //                     <button onClick={toggleSidebar} className='p-1.5 rounded-lg bg-purple-200 hover:bg-purpleHover hover:text-lightBlue'>
-        //                         <RxHamburgerMenu />
-        //                     </button>
-        //                 </div>
-        //             </div>
-        //             <div className='flex items-center rounded-md px-4 py-2 relative'>
-        //                 <BsSearch className='text-gray-400 text-md block float-left cursor-pointer absolute left-6 me-2'/>
-        //                 <input type={"search"} className='border-2 py-2 px-6 text-base bg-transparent w-full focus:outline-none  placeholder-gray-400' placeholder='search'/>      
-        //             </div>*/}
-{/* </div>
-        </nav> */}
--- src/app/components/SideBar.jsx
+++ src/app/components/SideBar.jsx
@@ -7,15 +7,8 @@ import data from './data';
 const SideBar = ({ collapsed }) => {
  
     return (
-        // w-80 max-w-7xl
         <aside className={`flex bg-white sidebar relative top-0 bottom-0 z-99 transiton-width duration-300 overflow-y-auto ${collapsed ? 'w-1/16' : 'w-80 md:block'}  `}>
-            {/* ${collapsed ? 'w-1/16' : 'w-80 md:block'} */}
             <nav className='h-screen flex flex-col'>
-                    {/* <span className='mb-3 hidden md:visible'>
-                        <a href="#" className='text-xl font-bold bg-purpleBackground py-4'>
-                            Berry
-                        </a>
-                    </span> */}
                 <ul className="flex-1 px-3">
                     {data.map((item, index) => (
                         <SideBarDynamic key={index} {...item} collapsed={collapsed} />
