96ccf838bfcfad35f28bbc37ff5e51f2c83186f6
senayakagunduz <senayakgndz@gmail.com>
2023-12-04 11:56:33 +0300

    margin padding added

--- src/components/SideBarItem.jsx
+++ src/components/SideBarItem.jsx
@@ -9,9 +9,9 @@ const SideBarItem = ({ icon, text, children, collapsed,leaf }) => {
   };
   
   return (
-    <li className={`flex-col items-center gap-2 cursor-pointer ${leaf ? 'ml-7 list-disc' : ''}`}>
+    <li className={`flex-col items-center gap-2 cursor-pointer ${leaf ? 'pl-0 ml-7 list-disc' : ''}`}>
       <div className="flex align-center justify-between gap-7 w-30 mb-3 text-textGrey hover:bg-purple-100  rounded hover-scale-710" >
-        <div onClick={toggleDropdown} className="flex py-3 pl-1 px-4 items-center justify-between gap-7 w-30">
+        <div onClick={toggleDropdown} className="flex py-3 pl-3 px-4 items-center justify-between gap-7 w-30">
           <div className="flex items-center">
             <span className="text-xl mr-3">{icon}</span>
             <span>{text}</span>
